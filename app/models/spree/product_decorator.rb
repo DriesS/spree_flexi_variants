a291b52479c418e0496a6553beecde6908521c8e
Dries Steenhouwer <steenhouwer.dries@gmail.com>
2013-01-09 15:54:09 +0100

    add clone products

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -9,15 +9,48 @@ module Spree
     # allowed customizations
     has_and_belongs_to_many :product_customization_types
 
+    attr_accessor :is_clone
 
     def duplicate_extra(original_product)
-      ad_hoc_option_types = original_product.ad_hoc_option_types.map { |ad_hoc_option_types| ad_hoc_new = ad_hoc_option_types.dup; ad_hoc_new.product_id = id; ad_hoc_new.created_at = ad_hoc_new.updated_at = nil; ad_hoc_new }
-      ad_hoc_variant_exclusions = original_product.ad_hoc_variant_exclusions.map { |exclusion| exclusion_new = exclusion.dup; exclusion_new.product_id = id; exclusion_new.created_at = exclusion_new.updated_at = nil; exclusion_new }
+
+      self.is_clone = true
+
+      self.ad_hoc_option_types = original_product.ad_hoc_option_types.map do |ad_hoc_option_type|
+        ad_hoc_new = ad_hoc_option_type.dup
+        ad_hoc_new.product_id = id
+        ad_hoc_new.created_at = ad_hoc_new.updated_at = nil
+        ad_hoc_new.ad_hoc_option_values = ad_hoc_option_type.ad_hoc_option_values.map do |ov|
+          new_ov = ov.dup
+          new_ov
+        end
+        ad_hoc_new
+      end
+
+      self.save!
+
+      self.ad_hoc_variant_exclusions = original_product.ad_hoc_variant_exclusions.map do |exclusion|
+        exclusion_new = exclusion.dup
+        exclusion_new.product_id = id
+        exclusion_new.created_at = exclusion_new.updated_at = nil
+        exclusion_new.save
+        exclusion_new.excluded_ad_hoc_option_values = exclusion.excluded_ad_hoc_option_values.map do |excluded_value|
+          new_excluded_value = excluded_value.dup
+          new_excluded_value.ad_hoc_option_value = AdHocOptionValue.joins(:ad_hoc_option_type).where("spree_ad_hoc_option_types.product_id = ?", self.id).where(:option_value_id => excluded_value.ad_hoc_option_value.option_value_id).first
+          new_excluded_value
+        end
+        exclusion_new
+      end
+
+      self.save!
+
+      self
+
     end
 
     private
 
     def attach_option_values(ad_hoc_option_type)
+      return if self.is_clone
       ad_hoc_option_type.option_type.option_values.each do |ov|
         ahot = AdHocOptionValue.new()
         ahot.option_value_id = ov.id
